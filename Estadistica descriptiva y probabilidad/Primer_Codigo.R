7cf466eef682e3f2d40e0f5265de40fde71fd382
Paola <montserrat_osorio@yahoo.com>
2024-08-28 15:21:41 -0600

    Primer codigo en R en vscode

--- Estadistica descriptiva y probabilidad/Primer_Codigo.R
+++ Estadistica descriptiva y probabilidad/Primer_Codigo.R
@@ -1,1 +1,18 @@
 library(jpeg)
+
+ruta<-file.choose()
+img <- readJPEG(ruta)
+R <- img[,,1]
+plot(0:1,0:1, type='n', axes = FALSE, xlab = "", ylab = "")
+rasterImage(R, 0, 0, 1, 1)
+T <- function(x){
+    r <- sum(R<=x)/(nrow(R)*ncol(R))
+    return(r)
+}
+res <- matrix(0,nrow = nrow(R), ncol = ncol(R))
+for (i in 1:nrow(R))
+for (j in 1:ncol(R)){
+    res[i,j] <- T(R[i,j])
+}
+plot(0:1,0:1, type='n', axes = FALSE, xlab = "", ylab = "")
+rasterImage(res, 0, 0, 1, 1)
