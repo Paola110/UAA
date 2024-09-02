48e09433040a9104af1ce0fb9794f610dd60c925
Paola <montserrat_osorio@yahoo.com>
2024-09-02 08:19:30 -0600

    En estadistica usamos el metodo de ecualizción de imagenes y en estructuras aplicamos el algoritmo de Heap

--- Estructuras Computacionales Avanzadas/Algoritmo_Heap_Permutaciones.cpp
+++ Estructuras Computacionales Avanzadas/Algoritmo_Heap_Permutaciones.cpp
@@ -5,15 +5,60 @@
 // Pérez López Pablo David
 // Reteguin Vanessa
 // Profesor Gutierrez Marfileño Luis Fernando
-// Aplicacion del algoritmo de Heap para el desarrollo de las permutaciones de un conjunto de elementos
+// Aplicacion del algoritmo de Heap para el desarrollo de las permutaciones de
+// un conjunto de elementos
 
 // Incluir librerias necesarias
 #include <iostream>
 using namespace std;
+int num;
 
 // Funcion Permutaciones
 
 // Programa principal con el menu para solicitar el conjunto de datos a permutar
+
+// Imprimir arreglo
+void printArrayString(string arreglo_permutar[], int num) {
+  int i;
+  for (i = 0; i < num; i++) {
+    cout << arreglo_permutar[i];
+    if (i != num - 1) {
+      cout << ", ";
+    }
+  }
+}
+
+// (Swap) Intercambiar elementos dentro del arreglo
+
+void swapElements(string arreglo_permutar[], int n1, int n2) {
+  string temp;
+  temp = arreglo_permutar[n1];
+  arreglo_permutar[n1] = arreglo_permutar[n2];
+  arreglo_permutar[n2] = temp;
+}
+
+// Generador de permutaciones
+void permutations(int nElements, string allElements[]) {
+  int i, n = num;
+
+  if (nElements == 1) {
+    return;
+  } else {
+    for (i = 0; i < nElements - 1; i++) {
+      permutations((nElements - 1), allElements);
+      if (nElements % 2 == 0) {
+        swapElements(allElements, i, (nElements - 1));
+
+      } else {
+        swapElements(allElements, 0, (nElements - 1));
+      }
+      cout << endl;
+      printArrayString(allElements, n);
+      cout << endl;
+    }
+    permutations((nElements - 1), allElements);
+  }
+}
 
 // Menú de opciones
 void diplayMenu() {
@@ -45,58 +90,84 @@ int main() {
       cin.ignore();
     }
 
-
     switch (userChoice) {
-    case 1: {
+      case 1: {
+        int i, integer;
 
-      cout << endl << "[1] Permutar elementos numericos.\n\n";
+        cout << endl << "[1] Permutar elementos numericos.\n\n";
 
-      int num = 0; // Tamaño del arreglo
-      cout << endl << "Ingresa el numero de elementos a permutar, es decir, la magnitud del arreglo: ";
-      while ((!(cin >> num)) && (num <= 10)) {
+        num = 0;  // Tamaño del arreglo
+        cout << endl
+             << "Ingresa el numero de elementos a permutar, es decir, la "
+                "magnitud del arreglo: ";
+        while (!((cin >> num) && (num >= 2 && num <= 10))) {
           cin.clear();
           cin.ignore();
-      }
-      int arreglo_permutar[num]; // Arreglo a permutar
-      cout << endl << "Ingresa los "<< num <<" elementos del arreglo separados por enter:\n";
-      for (int i=0 ; i<num ; i++){
-        cout <<"["<< i+1 << "] --> ";
-        while (!(cin >> arreglo_permutar[i])) {
+        }
+        string arreglo_permutar[num];  // Arreglo a permutar
+        cout << endl
+             << "Ingresa los " << num
+             << " elementos del arreglo separados por enter:\n";
+        for (i = 0; i < num; i++) {
+          cout << "[" << i + 1 << "] --> ";
+          while (!(cin >> integer)) {
             cin.clear();
             cin.ignore();
+          }
+          arreglo_permutar[i] = to_string(integer);
         }
+
+        cout << endl << "Arreglo: ";
+        printArrayString(arreglo_permutar, num);
+
+        cout << endl << "Permutaciones: ";
+        cout << endl;
+        printArrayString(arreglo_permutar, num);
+        cout << endl;
+        permutations(num, arreglo_permutar);
+
+        break;
       }
+      case 2: {
+        int i;
+        cout << endl << "[2] Permutar elementos alfanumericos.\n\n";
 
-      break;
-    }
-    case 2:
-      {
-
-      cout << endl << "[2] Permutar elementos alfanumericos.\n\n";
-
-      int num = 0; // Tamaño del arreglo
-      cout << endl << "Ingresa el numero de elementos a permutar, es decir, la magnitud del arreglo: ";
-      while ((!(cin >> num)) && (num <= 10)) {
+        num = 0;  // Tamaño del arreglo
+        cout << endl
+             << "Ingresa el numero de elementos a permutar, es decir, la "
+                "magnitud del arreglo: ";
+        while (!((cin >> num) && (num >= 2 && num <= 10))) {
           cin.clear();
           cin.ignore();
-      }
-      char arreglo_permutar[num]; // Arreglo a permutar
-      cout << endl << "Ingresa los "<< num <<" elementos del arreglo separados por enter:\n";
-      for (int i=0 ; i<num ; i++){
-        cout <<"["<< i+1 << "] --> ";
-        while (!(cin >> arreglo_permutar[i])) {
+        }
+        string arreglo_permutar[num];  // Arreglo a permutar
+        cout << endl
+             << "Ingresa los " << num
+             << " elementos del arreglo separados por enter:\n";
+        for (i = 0; i < num; i++) {
+          cout << "[" << i + 1 << "] --> ";
+          while (!(cin >> arreglo_permutar[i])) {
             cin.clear();
             cin.ignore();
+          }
         }
+
+        cout << endl << "Arreglo: ";
+        printArrayString(arreglo_permutar, num);
+
+        cout << endl << "Permutaciones: ";
+        cout << endl;
+        printArrayString(arreglo_permutar, num);
+        cout << endl;
+        permutations(num, arreglo_permutar);
+
+        break;
       }
 
-      break;
-    }
-
-    case 3:
-      endTitle();
-      run = false;
-      break;
+      case 3:
+        endTitle();
+        run = false;
+        break;
     }
   }
 
