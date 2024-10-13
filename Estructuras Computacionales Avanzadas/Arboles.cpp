be473d6999af9264a3d35a5d4c46410530d1f034
Paola <montserrat_osorio@yahoo.com>
2024-10-13 00:26:50 -0600

    Configuracion de kernel e implementacion de codigo para obtener firmas de un cheque

--- Estructuras Computacionales Avanzadas/Arboles.cpp
+++ Estructuras Computacionales Avanzadas/Arboles.cpp
@@ -1,228 +1,414 @@
 /*
- * +----------------------------------------------------------------------------+
- * | CARDUI WORKS v0.0.2
- * +----------------------------------------------------------------------------+
- * | Copyright (c) 2024 - 2024, CARDUI.COM (www.cardui.com)
- * | Vanessa Reteguín <vanessa@reteguin.com>
- * | Released under the MIT license
- * | www.cardui.com/carduiframework/license/license.txt
- * +----------------------------------------------------------------------------+
- * | Author.......: Vanessa Retegín <vanessa@reteguin.com>
- * | First release: June 7th, 2024
- * | Last update..: June 9th, 2024
- * | WhatIs.......: Binary Tree: exercise - Main
- * +----------------------------------------------------------------------------+
- * | University...: Universidad Autónoma de Aguascalientes
- * | Major........: Ingeniería en Computación Inteligente (ICI)
- * | Semester.....: Semestre II
- * | Section......: Group 2 - A
- * | College ID...: 375533
- * | Subject......: Estructuras Computacionales
- * | Professor....: María Loreto Edit López Veloz
- * | Spanish Title: Árboles Binarios: ejercicio - Main
- * +----------------------------------------------------------------------------+
- */
+11 octubre de 2024
+Paola Osorio - 216511
+Pablo David Pérez López - 300452
+Vanessa Reteguín - 373355
 
-#include <stdlib.h>  //srand
-#include <time.h>    //time
+Práctica #3
+
+Universidad Aútonoma de Aguascalientes
+Ingeniería en Computación Inteligente (ICI)
+Semestre II
+Grupo 2 - E
+Estructuras Computacionales avanzadas
+Luis Fernando Gutiérrez Marfileño
+*/
+
+#include <stdlib.h> //srand
+#include <time.h>   //time
+#include <array>    // sizeof()
 
 #include <iostream>
 using namespace std;
 
-struct node {
-  int data;
-  struct node *left;
-  struct node *right;
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
 };
 
-int randomNum(int lower, int upper) {
-  int num;
-  num = (rand() % (upper - lower + 1)) + lower;
-  return num;
+void preOrder(node *);
+
+void inOrder(node *tree);
+
+void postOrder(node *tree);
+
+int aux_X = 0; // Variable auxiliar para controlar la posición horizontal de los nodos
+
+int randomNum(int lower, int upper)
+{
+    int num;
+    num = (rand() % (upper - lower + 1)) + lower;
+    return num;
 }
 
-void diplayMenu() {
-  cout << endl
-       << "\n.--------------------."
-          "\n||    -{ MENU }-    ||"
-          "\n.--------------------."
-          "\n| [1] Agregar nodo   |"
-          "\n| [2] Nodo aleatorio |"
-          "\n| [3] Eliminar nodo  |"
-          "\n| [4] Buscar nodo    |"
-          "\n| [5] Mostrar árbol  |"
-          "\n|          [6] Salir |"
-          "\n.--------------------.\n";
+void diplayMenu()
+{
+    cout << endl
+         << "\n.--------------------."
+            "\n||    -{ MENU }-    ||"
+            "\n.--------------------."
+            "\n| [1] Agregar nodo   |"
+            "\n| [2] Nodo aleatorio |"
+            "\n| [3] Eliminar nodo  |"
+            "\n| [4] Buscar nodo    |"
+            "\n| [5] Mostrar árbol  |"
+            "\n.--------------------."
+            "\n| Recorrido en:      |"
+            "\n|      [6] Preorden  |"
+            "\n|      [7] Inorden   |"
+            "\n|      [8] Postorden |"
+            "\n.--------------------."
+            "\n|          [9] Salir |"
+            "\n.--------------------.\n";
 }
 
-void endTitle() {
-  cout << "\n  ^~^  , * ------------- *"
-          "\n ('Y') ) |  Hasta luego! | "
-          "\n /   \\/  * ------------- *"
-          "\n(\\|||/)        FIN      \n";
+void endTitle()
+{
+    cout << "\n  ^~^  , * ------------- *"
+            "\n ('Y') ) |  Hasta luego! | "
+            "\n /   \\/  * ------------- *"
+            "\n(\\|||/)        FIN      \n";
 }
 
-node *createNode(int n) {
-  node *newNode = new (node);
-  newNode->left = NULL;
-  newNode->right = NULL;
-  newNode->data = n;
+node *createNode(int n)
+{
+    node *newNode = new (node);
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->data = n;
 
-  return newNode;
+    return newNode;
 }
 
-void newChildNode(node *&root, int n) {
-  if (root == NULL) {
-    root = createNode(n);
-  } else {
-    if (n < root->data) {
-      newChildNode(root->left, n);
-    } else {
-      newChildNode(root->right, n);
+void newChildNode(node *&root, int n)
+{
+    if (root == NULL)
+    {
+        root = createNode(n);
     }
-  }
-}
-
-void printTree(node *node, int level) {
-  int i;
-
-  if (node == NULL) {
-    return;
-  } else {
-    printTree(node->right, level + 1);
-    for (i = 0; i < level; i++) {
-      cout << "    ";
-    }
-    cout << node->data << endl;
-    printTree(node->left, level + 1);
-  }
-}
-
-bool search(node *root, int seachedValue) {
-  if (root == NULL) {
-    return false;
-  } else if (root->data == seachedValue) {
-    return true;
-  } else if (seachedValue < root->data) {
-    return search(root->left, seachedValue);
-  } else {
-    return search(root->right, seachedValue);
-  }
-}
-
-void printSeachedValue(node *node, int level, int seachedValue) {
-  int i;
-
-  if (node == NULL) {
-    return;
-  } else {
-    printSeachedValue(node->right, level + 1, seachedValue);
-    for (i = 0; i < level; i++) {
-      cout << "    ";
-    }
-    if (node->data == seachedValue) {
-      cout << "->{" << node->data << "}<-" << endl;
-    } else {
-      cout << node->data << endl;
-    }
-    printSeachedValue(node->left, level + 1, seachedValue);
-  }
-}
-
-bool deleteNode(node *root, int valueToDelete) {
-  if (root == NULL) {
-    return false;
-  } else if (root->data == valueToDelete) {
-    delete root;
-    return true;
-  } else if (valueToDelete < root->data) {
-    return search(root->left, valueToDelete);
-  } else {
-    return search(root->right, valueToDelete);
-  }
-}
-
-int main() {
-  srand(time(NULL));
-
-  // Menu var
-  int userChoice;
-  bool run = true;
-
-  // Aux var
-  int n, i, cont = 0;
-
-  // Root Node
-  node *root = NULL;
-
-  for (i = 0; i < 5; i++) {
-    n = randomNum(-20, 20);
-    newChildNode(root, n);
-  }
-
-  while (run == true) {
-    diplayMenu();
-    while (!((cin >> userChoice) && (userChoice >= 1 && userChoice <= 6))) {
-      cin.clear();
-      cin.ignore();
-    }
-
-    switch (userChoice) {
-      case 1:
-        cout << "Ingrese el valor del nodo: ";
-        while (!(cin >> n)) {
-          cin.clear();
-          cin.ignore();
+    else
+    {
+        if (root->left == NULL)
+        {
+            root->left = createNode(n);
         }
+        else if (root->right == NULL)
+        {
+            root->right = createNode(n);
+        }
+        else
+        {
+            if (n < root->data)
+            {
+                newChildNode(root->left, n);
+            }
+            else
+            {
+                newChildNode(root->right, n);
+            }
+        }
+    }
+}
+
+void printTree(node *node, int level)
+{
+    int i;
+
+    if (node == NULL)
+    {
+        return;
+    }
+    else
+    {
+        printTree(node->right, level + 1);
+        for (i = 0; i < level; i++)
+        {
+            cout << "    ";
+        }
+        cout << node->data << endl;
+        printTree(node->left, level + 1);
+    }
+}
+
+void gotoxy(int x, int y)
+{
+    printf("%c[%d;%df", 0x1B, y, x);
+
+    // Solución de boodahDEV
+    //  https://gist.github.com/boodahDEV/7e10925c88d487cb5cd6770a23ac3f15
+}
+
+// Función para mostrar el árbol de forma gráfica. Créditos: Alan García
+void showTree(node *root, int aux_Y)
+{ // aux_Y representa el nivel del árbol
+    if (root == nullptr)
+        return;
+
+    aux_X += 4; // Variable que permite posicionar en el eje X
+
+    showTree(root->left, aux_Y + 2); // Se para hasta el nodo más a la izquierda del árbol (subárbol izquierdo)
+
+    gotoxy(8 + aux_X - aux_Y, 15 + aux_Y); // Posiciona el nodo según sus coordenadas en X y en Y
+
+    cout << root->data << endl
+         << endl; // Muestra el dato del node respectivo
+
+    showTree(root->right, aux_Y + 2); // Se para hasta el nodo más a la derecha del árbol (subárbol derecho)
+}
+
+void printTreeVertical(node *root)
+{
+    cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+    showTree(root, 6);
+    aux_X = 0;
+}
+
+void printTreeHorzontal(node *node, int level)
+{
+    int i;
+
+    if (node == NULL)
+    {
+        return;
+    }
+    else
+    {
+        printTreeHorzontal(node->right, level + 1);
+        for (i = 0; i < level; i++)
+        {
+            cout << "    ";
+        }
+        cout << node->data << endl;
+        printTreeHorzontal(node->left, level + 1);
+    }
+}
+
+bool search(node *root, int seachedValue)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    else if (root->data == seachedValue)
+    {
+        return true;
+    }
+    else if (seachedValue < root->data)
+    {
+        return search(root->left, seachedValue);
+    }
+    else
+    {
+        return search(root->right, seachedValue);
+    }
+}
+
+void printSeachedValue(node *node, int level, int seachedValue)
+{
+    int i;
+
+    if (node == NULL)
+    {
+        return;
+    }
+    else
+    {
+        printSeachedValue(node->right, level + 1, seachedValue);
+        for (i = 0; i < level; i++)
+        {
+            cout << "    ";
+        }
+        if (node->data == seachedValue)
+        {
+            cout << "->{" << node->data << "}<-" << endl;
+        }
+        else
+        {
+            cout << node->data << endl;
+        }
+        printSeachedValue(node->left, level + 1, seachedValue);
+    }
+}
+
+bool deleteNode(node *root, int valueToDelete)
+{
+    if (root == NULL)
+    {
+        return false;
+    }
+    else if (root->data == valueToDelete)
+    {
+        delete root;
+        return true;
+    }
+    else if (valueToDelete < root->data)
+    {
+        return search(root->left, valueToDelete);
+    }
+    else
+    {
+        return search(root->right, valueToDelete);
+    }
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    // Menu var
+    int userChoice;
+    bool run = true;
+
+    // Aux var
+    int n, i, cont = 0;
+
+    // Root Node
+    node *root = NULL;
+
+    int numbers[] = {8, 4, 13, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 15, 13};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    for (i = 0; i < size; i++)
+    {
+        n = numbers[i];
         newChildNode(root, n);
-        printTree(root, cont);
-        break;
-
-      case 2:
-        n = randomNum(-20, 20);
-        newChildNode(root, n);
-        printTree(root, cont);
-        break;
-
-      case 3:
-        cout << "Ingrese un valor a eliminar dentro del árbol: ";
-        while (!(cin >> n)) {
-          cin.clear();
-          cin.ignore();
-        }
-        if (deleteNode(root, n) == true) {
-          cout << endl << "[*] Eliminado exitosamente:" << endl;
-          printTree(root, cont);
-        } else {
-          cout << endl << "[!] Valor no encontrado dentro del árbol" << endl;
-        }
-        break;
-
-      case 4:
-        cout << "Ingrese un valor a buscar dentro del árbol: ";
-        while (!(cin >> n)) {
-          cin.clear();
-          cin.ignore();
-        }
-        if (search(root, n) == true) {
-          cout << endl << "[*] Valor encontrado:" << endl;
-          printSeachedValue(root, cont, n);
-        } else {
-          cout << endl << "[!] Valor no encontrado dentro del árbol" << endl;
-        }
-        break;
-
-      case 5:
-        cout << endl << "Árbol\n" << endl;
-        printTree(root, 0);
-        break;
-
-      case 6:
-        endTitle();
-        run = false;
-        break;
     }
-  }
 
-  return 0;
+    while (run == true)
+    {
+        diplayMenu();
+        while (!((cin >> userChoice) && (userChoice >= 1 && userChoice <= 9)))
+        {
+            cin.clear();
+            cin.ignore();
+        }
+
+        switch (userChoice)
+        {
+        case 1:
+            cout << "Ingrese el valor del nodo: ";
+            while (!(cin >> n))
+            {
+                cin.clear();
+                cin.ignore();
+            }
+            newChildNode(root, n);
+            printTreeVertical(root);
+            break;
+
+        case 2:
+            n = randomNum(-20, 20);
+            newChildNode(root, n);
+            printTreeVertical(root);
+            break;
+
+        case 3:
+            cout << "Ingrese un valor a eliminar dentro del árbol: ";
+            while (!(cin >> n))
+            {
+                cin.clear();
+                cin.ignore();
+            }
+            if (deleteNode(root, n) == true)
+            {
+                cout << endl
+                     << "[*] Eliminado exitosamente:" << endl;
+                cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+                printTreeVertical(root);
+            }
+            else
+            {
+                cout << endl
+                     << "[!] Valor no encontrado dentro del árbol" << endl;
+            }
+            break;
+
+        case 4:
+            cout << "Ingrese un valor a buscar dentro del árbol: ";
+            while (!(cin >> n))
+            {
+                cin.clear();
+                cin.ignore();
+            }
+            if (search(root, n) == true)
+            {
+                cout << endl
+                     << "[*] Valor encontrado:" << endl;
+                printSeachedValue(root, cont, n);
+            }
+            else
+            {
+                cout << endl
+                     << "[!] Valor no encontrado dentro del árbol" << endl;
+            }
+            break;
+
+        case 5:
+            cout << endl
+                 << "Árbol\n"
+                 << endl;
+            printTreeVertical(root);
+            break;
+
+        case 6:
+            cout << endl
+                 << "Preorden\n"
+                 << endl;
+            preOrder(root);
+            break;
+
+        case 7:
+            cout << endl
+                 << "Inorden\n"
+                 << endl;
+            inOrder(root);
+            break;
+
+        case 8:
+            cout << endl
+                 << "Postorden\n"
+                 << endl;
+            postOrder(root);
+            break;
+
+        case 9:
+            endTitle();
+            run = false;
+            break;
+        }
+    }
+
+    return 0;
+}
+
+void preOrder(node *tree){
+    if(tree == NULL){
+        return;
+    }else{
+        cout << tree -> data << " - ";
+        preOrder(tree -> left);
+        preOrder(tree -> right);
+    }
+}
+
+void inOrder(node *tree){
+    if(tree == NULL){
+        return;
+    }else{
+        inOrder(tree -> left);
+        cout << tree -> data << " - ";
+        inOrder(tree -> right);
+    }
+}
+
+void postOrder(node *tree){
+    if(tree == NULL){
+        return;
+    }else{
+        postOrder(tree -> left);
+        postOrder(tree -> right);
+        cout << tree -> data << " - ";
+    }
 }
